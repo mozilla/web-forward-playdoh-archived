04ff0a7f369c73575f40d07d80bcc7f4ee8fa21c
Matthew Riley MacPherson <mmacpherson@mozilla.com>
2011-07-22 17:16:47 -0300

    Update form/model/sql fields to display updated form info and copy

--- apps/apply/models.py
+++ apps/apply/models.py
@@ -16,43 +16,80 @@ AREAS_OF_INTEREST = (
     ('Core', 'Core'),
 )
 
+PROGRAMS = (
+    ('6-month Fellows Program', '6-month Fellows Program'),
+    ('4-week Bootcamp', '4-week Bootcamp'),
+    ("Don't know - you tell me", "Don't know - you tell me"),
+)
+
 class Application(models.Model):
     """
     An application to Web Forward containing personal information about the
     submitter and details of the propsed incubator project.
     """
+    program_selection = models.CharField(max_length=50,
+                                         choices=PROGRAMS,
+                                         verbose_name='Which program do you want to apply for?')
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(verbose_name='Email Address')
     phone_number = models.CharField(max_length=30)
-    teammates = models.TextField(verbose_name='Names of all Teammates')
-    video_url = models.URLField(verbose_name='Video Intro',
-                                verify_exists=False)
-    pitch = models.TextField(verbose_name='What is your team going to build?')
-    interest = models.CharField(max_length=50, choices=AREAS_OF_INTEREST,
-                                verbose_name='Within which of our interest areas does your solution play?')
 
+    teammates = models.TextField(verbose_name='Names of all Teammates',
+                                 blank=True)
     team_info = models.TextField(verbose_name='For each of you, please list your name, age, year of graduation, school, degree and subject for each degree, email address, personal url, github url, employer and title (if applicable). Separate team members with blank lines.')
-    team_accomplishments = models.TextField(verbose_name="Tells us a bit about yourself - what's the most amazing thing each of you has done so far? Separate team members with blank lines and start each answer with your respective name.")
-    team_history = models.TextField(verbose_name='How long have you known each other, have you all met in person yet and what have you done together before? (If you have stuff on the Web: add an URL.)')
-
-    reason_for_idea = models.TextField(verbose_name='Why did you choose this idea to work on? Why do you think you will be successful with your idea?')
-    uniqueness_of_idea = models.TextField(verbose_name="What's new and/or different about your product? What do people today without your product?")
-    idea_relevance = models.TextField(verbose_name='Why does your product matter?')
-    why_mozilla = models.TextField(verbose_name='Why do you think Mozilla is the right place for you?')
-    progress = models.TextField(verbose_name="If you've already started working on your product, how long have you been working and how many lines of code (if applicable) have you written?")
-    launch_info = models.TextField(verbose_name="How far along are you? Do you have a alpha/beta/v1 yet? If not, when will you? Are you launched? If so, how many users do you have? If you're launched, what is your monthly growth rate?")
-    demo_url = models.URLField(verbose_name="If you have an online demo, what's the URL?", verify_exists=False, blank=True)
-    team_exclusive_members = models.TextField(verbose_name='If we bring you into the program, who of you will commit working exclusively (no school, no other jobs) on this project for the duration of the project?')
-    team_non_exclusive_members = models.TextField(verbose_name="For the people on your team who can't/don't want to commit full-time: Why not? What level of commitment are you willing to make?")
-    team_prior_commitments = models.TextField(verbose_name='Do any of you have commitments in the future (e.g. finishing college, going to grad school), and if so what?')
     location = models.TextField(verbose_name='Where do you live now?')
 
-    legal = models.TextField(verbose_name='Are any of you covered by noncompetes or intellectual property agreements which overlap with your project?')
-    source_code_info = models.TextField(verbose_name='Was any of your code written by someone who is not one of your founders? If so, how can you safely use it? (Open Source is fine.)')
+    video_url = models.URLField(verbose_name="Video Intro: Please send a brief video introducing yourself and your project. We don't care about the quality of the video ((hone video is fine), we just want to see and hear you.",
+                                verify_exists=False, blank=True)
+
+    pitch = models.TextField(verbose_name='What are you building? What is your product about?')
+    idea_relevance = models.TextField(verbose_name='Why do you think your product matters? Why should anyone care?')
+
+    interest = models.CharField(max_length=50, choices=AREAS_OF_INTEREST,
+                                verbose_name='The WebFWD site lists areas of interest to Mozilla and relevant to the open, decentralized Web. How does your solution relate to these interest areas?')
+
+    benefit = models.TextField(verbose_name='How do you hope to benefit from WebFWD?')
+
+    progress = models.TextField(verbose_name="How long have you been working on your product and how many lines of code (if applicable) have been written?")
+
+    launch_info = models.TextField(verbose_name="How far along are you? Do you have a alpha/beta/v1 yet? If not, when will you? Are you launched? If so, how many users do you have? If you've launched, what is your monthly growth rate?")
+
+    demo_url = models.URLField(verbose_name="If you have an online demo, what's the URL?", verify_exists=False, blank=True)
+
+    team_prior_commitments = models.TextField(verbose_name='Please describe any upcoming future commitments such as finishing college, going to grad school, or moving to a new location.',
+                                              blank=True)
+
+    source_code_info = models.TextField(verbose_name='Was any of your code written by someone who is not one of your founders or team members? If so, how can you safely use it? (Open Source is fine.)',
+                                        blank=True)
+
+    team_accomplishments = models.TextField(verbose_name="Tell us a bit more about yourself. What's the most amazing thing you've done so far? Separate team members with blank lines and submit individual answers with each person's name.")
+
+    team_history = models.TextField(verbose_name="For team applications: Please tell us how long you've known each other, whether you've met in person, and whether you've worked together on other projects. (If you have stuff on the Web, please share an URL.)",
+                                    blank=True)
 
     anecdote = models.TextField(verbose_name='Please tell us something surprising or amusing that one of you has discovered. (The answer need not be related to your project.)')
-    other = models.TextField(verbose_name='Anything else we should or need to know?')
+
+    other = models.TextField(verbose_name='Is there anything else we should know about you, your team, or your project?',
+                             blank=True)
+
+    want_designer = models.BooleanField(default=False, verbose_name="I'm interested in getting matched to a UI/UX designer.")
+
+    # Legacy model fields that aren't used anymore or shown in forms.
+    # Previous submissions have these fields though, so we keep them around
+    # so the data is preserved.
+    reason_for_idea = models.TextField(blank=True,
+                                       verbose_name='Why did you choose this idea to work on? Why do you think you will be successful with your idea?')
+    uniqueness_of_idea = models.TextField(blank=True,
+                                          verbose_name="What's new and/or different about your product? What do people today without your product?")
+    why_mozilla = models.TextField(blank=True,
+                                   verbose_name='Why do you think Mozilla is the right place for you?')
+    team_exclusive_members = models.TextField(blank=True,
+                                              verbose_name='If we bring you into the program, who of you will commit working exclusively (no school, no other jobs) on this project for the duration of the project?')
+    team_non_exclusive_members = models.TextField(blank=True,
+                                                  verbose_name="For the people on your team who can't/don't want to commit full-time: Why not? What level of commitment are you willing to make?")
+    legal = models.TextField(blank=True, verbose_name='Are any of you covered by noncompetes or intellectual property agreements which overlap with your project?')
 
     # Admin-only fields; not presented to the user and ignored in
     # the ApplicationForm class.
